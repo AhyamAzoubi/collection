38c8c69b554bc19b18dc18e23d72a8fdf76dec5a
Ahyam Azoubi <ahyam.azoubi@gmail.com>
2022-10-26 15:07:44 +0200

    cart

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -13,52 +13,69 @@ const Cart = () => {
   }, 0);
 
   return (
-    <div className="container ">
+    <div className="row mx-3 my-3 position-relative ">
       {state.map((product, index) => {
         return (
-          <div className="card mb-3" key={index}>
-            <img className="card-img-top " src={product.image} alt="" />
-            <div className="card-body">
-              <h5 className="card-title">{product.title}</h5>
-              <p className="card-text">{product.quantity * product.price}$</p>
-
-              <button
-                className="btn btn-dark"
-                onClick={() => dispatch({ type: "increase", payload: product })}
-              >
-                +
-              </button>
-
-              <p>{product.quantity}</p>
-
-              <button
-                className="btn btn-dark"
-                onClick={() => {
-                  if (product.quantity > 1) {
-                    dispatch({ type: "decrease", payload: product });
-                  } else {
-                    dispatch({ type: "remove", payload: product });
+          <div
+            className="card my-2 mx-2 "
+            style={{ width: "10rem" }}
+            key={index}
+          >
+            <img
+              className="img-top "
+              style={{ height: "9rem", width: "9rem" }}
+              src={product.image}
+              alt=""
+            />
+            <div className="">
+              <h5 className="card-title fs-6">{product.title}</h5>
+              <p className="card-text text-center fs-4">
+                {product.quantity * product.price}$
+              </p>
+              <div className="d-flex justify-content-evenly">
+                <button
+                  className="btn btn-outline-dark"
+                  onClick={() =>
+                    dispatch({ type: "increase", payload: product })
                   }
-                }}
-              >
-                -
-              </button>
+                >
+                  +
+                </button>
+
+                <p>{product.quantity}</p>
+
+                <button
+                  className="btn btn-outline-dark"
+                  onClick={() => {
+                    if (product.quantity > 1) {
+                      dispatch({ type: "decrease", payload: product });
+                    } else {
+                      dispatch({ type: "remove", payload: product });
+                    }
+                  }}
+                >
+                  -
+                </button>
+              </div>
 
               <h1
+                className="text-center"
                 onClick={() => dispatch({ type: "remove", payload: product })}
               >
-                <FaRegTrashAlt className="remove" />
+                <FaRegTrashAlt className="remove " />
               </h1>
             </div>
           </div>
         );
       })}
-
-      {state.length > 0 && (
-        <div className="total">
-          <h2>Total : {total}</h2>
-        </div>
-      )}
+      <hr />
+      <div className="">
+        {state.length > 0 && (
+          <div className="total">
+            <h2>Total : {total} $</h2>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
